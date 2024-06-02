a015e5779e4f84f45d69d6852bf1fdf63ec7c2f3
RS-233 <146667095+RS-233@users.noreply.github.com>
2024-06-02 21:30:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = 4000
 //middleware
 app.use(express.json())
 app.use(cors({
-   origin: `http://yum-runfront.vercel.app/`
+   origin: `https://yum-runfront.vercel.app/`
 }))
 
 // DB Connection
