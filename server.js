948e785ef0a37de714c7ab6734bcb47c896cea7a
RS-233 <146667095+RS-233@users.noreply.github.com>
2024-06-03 12:34:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,21 +14,10 @@ const app = express()
 const port = 4000
 
 //middleware
-const allowedOrigins = [
-    'ttps://yum-runfront.vercel.app',
-    'https://admin-gamma-opal.vercel.app'
-];
-
 app.use(express.json())
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+   origin: '*'
+}))
 
 // DB Connection
 connectDB();
